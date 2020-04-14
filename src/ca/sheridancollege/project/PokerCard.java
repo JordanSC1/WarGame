e9b2f9d1d3bd07f4458d2756381b6c988bd505b0
JordanSC1 <grahajor@sheridancollege.ca>
2020-04-14 19:55:43 -0400

    Group Project Starter Code and Class Diagram

---
+++ src/ca/sheridancollege/project/PokerCard.java
@@ -0,0 +1,22 @@
+/*
+    [Class name]
+    Author: [the name of the person who created the class. It might be different from yours]
+    Date: [date]
+
+    Description
+    [Description about the class]
+    */
+
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author jrgra
+ */
+public class PokerCard extends Card {
+    
+    @Override
+    public String toString(){
+        
+    }
+}
